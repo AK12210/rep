20bdaa9d7f57af90f46ea2fcad18d393d8f375dc
AK12210 <156789576+AK12210@users.noreply.github.com>
2024-02-13 00:37:51 +0600

    Update functions1.py

--- functions1.py
+++ functions1.py
@@ -60,7 +60,6 @@ print(re(s))
 def has_33(s):
 	b = 0
 	for i in range(len(s)):
-		print(s[i])
 		if b != 0:
 			if s[i - 1] == s[i] and s[i] == 3:
 				return True
@@ -69,4 +68,70 @@ def has_33(s):
 
 s = [int(i) for i in input().split()]
 print(has_33(s))
+#task8
+def spy_game(s):
+	for i in range(len(s) - 2):
+		if s[i] == 0 and s[i + 1] == 0 and s[i + 2] == 7:
+			return True
+	return False
 
+s = [int(i) for i in input().split()]
+print(spy_game(s))
+#task9
+import math
+def vol(r):
+	return (4 / 3) * math.pi * r * r * r
+
+r = float(input())
+print(vol(r))
+#task10
+def dis(s):
+	s1 = []
+	for i in s:
+		if i not in s1:
+			s1.append(i)
+	return s1
+
+s = [int(i) for i in input().split()]
+print(dis(s))
+#task11
+def pal(s):
+	return s == s[::-1]
+
+s = input()
+print(pal(s))
+#task12
+def histogram(s):
+	for i in s:
+		j = i
+		s1 = ''
+		while j > 0:
+			s1 += '*'
+			j -= 1
+		print(s1)
+
+s = [int(i) for i in input().split()]
+histogram(s)
+#task13
+import random
+
+s = input('Hello! What is your name?')
+print('Well, ' + s + ', I am thinking of a number between 1 and 20.')
+n = random.randint(1, 20)
+print('Take a guess.')
+j = 0
+while True:
+	m = int(input())
+	if m < n:
+		print('Your guess is too low.')
+		print('Take a guess.')
+		j += 1
+	elif m > n:
+		print('Your guess is too high.')
+		print('Take a guess.')
+		j += 1
+	else:
+		print('Good job, ' + s + '! You guessed my number in ', end='')
+		print(j, end=' ')
+		print('guesses!')
+		
