d2baf439d162968c34a99b2c71ddc24c535ff560
AK12210 <156789576+AK12210@users.noreply.github.com>
2024-02-13 11:14:45 +0600

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,28 @@
+#task1
+class cs():
+    def __init__(self):
+        self.str1 = ""
+
+    def get_String(self):
+        self.str1 = input()
+
+    def print_String(self):
+        print(self.str1.upper())
+
+#task2
+class Shape(object):
+    def __init__(self):
+        pass
+
+    def area(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, l):
+        Shape.__init__(self)
+        self.length = l
+
+    def area(self):
+        return self.length * self.length
+
+  
