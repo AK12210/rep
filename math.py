f426dea6803db6bdde170cfc2b66114ca41befb2
AK12210 <156789576+AK12210@users.noreply.github.com>
2024-02-20 17:39:05 +0600

    Create math.py

---
+++ math.py
@@ -0,0 +1,25 @@
+#task1
+import math
+
+d = float(input())
+print(d * (math.pi / 180))
+
+#task2
+import math
+
+h = float(input())
+a = float(input())
+b = float(input())
+print((1 / 2) * h * (a + b))
+
+#task3
+from math import tan, pi
+
+n = int(input())
+s = int(input())
+print(n * (s * s) / (4 * tan(pi / n)))
+
+#task4
+a = float(input())
+h = float(input())
+print(a * h)
