bfa888ba861019c4f6dab73eea7320c45ba938fd
AK12210 <156789576+AK12210@users.noreply.github.com>
2024-02-20 16:26:46 +0600

    Update date.py

--- date.py
+++ date.py
@@ -1,10 +1,10 @@
 #task1
-import datetime
+from datetime import date, timedelta
 
 print(date.today() - timedelta(5))
 
 #task2
-import datetime
+from datetime import date, timedelta
 
 print(date.today() - timedelta(1), date.today(), date.today() + timedelta(1))
 
@@ -12,3 +12,10 @@ print(date.today() - timedelta(1), date.today(), date.today() + timedelta(1))
 import datetime
 
 print(datetime.datetime.today().replace(microsecond=0))
+#task4
+from datetime import datetime, time
+
+date1 = datetime.strptime('2024-02-18 10:32:25', '%Y-%m-%d %H:%M:%S')
+date2 = datetime.now()
+td = date2 - date1
+print(td.days * 24 * 3600 + td.seconds)
