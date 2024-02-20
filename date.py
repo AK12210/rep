a0734527254a16ebb1bda36f4f375737a0558507
AK12210 <156789576+AK12210@users.noreply.github.com>
2024-02-20 15:31:37 +0600

    Create date.py

---
+++ date.py
@@ -0,0 +1,14 @@
+#task1
+import datetime
+
+print(date.today() - timedelta(5))
+
+#task2
+import datetime
+
+print(date.today() - timedelta(1), date.today(), date.today() + timedelta(1))
+
+#task3
+import datetime
+
+print(datetime.datetime.today().replace(microsecond=0))
