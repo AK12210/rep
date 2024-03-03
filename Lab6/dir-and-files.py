85df56bbda75de8c5df35f01e2f18da8184a1326
AK12210 <156789576+AK12210@users.noreply.github.com>
2024-03-03 22:57:46 +0500

    Create dir-and-files.py

---
+++ Lab6/dir-and-files.py
@@ -0,0 +1,33 @@
+#task1
+import os
+path = 'C:\Users\Adm\Downloads'
+print([ name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name)) ])
+print([ name for name in os.listdir(path) if not os.path.isdir(os.path.join(path, name)) ])
+print([ name for name in os.listdir(path)])
+
+#task2
+import os
+print(os.access('C:\Users\Adm\Downloads\exampl.txt', os.F_OK))
+print(os.access('C:\Users\Adm\Downloads\exampl.txt', os.R_OK))
+print(os.access('C:\Users\Adm\Downloads\exampl.txt', os.W_OK))
+print(os.access('C:\Users\Adm\Downloads\exampl.txt', os.X_OK))
+
+#task3
+import os
+path = r'C:\Users\Adm\Downloads'
+print(os.path.exists(path))
+if os.path.exists(path) == True:
+	print(os.path.basename(path))
+	print(os.path.dirname(path))
+
+#task4
+with open(r"exampl.txt", 'r') as ff:
+    lin = len(ff.readlines())
+    print(lin)
+
+#task5
+s = ["Hello\n", "World\n"]
+ 
+ff = open('Adm\Downloads', 'w')
+ff.writelines(s)
+ff.close()
